cdadebe1deaf29e42ba9610953c33e6b6c1fc116
Hieu <piutar>
2020-10-07 11:53:52 +0700

    khoi tao lai

---
+++ src/test/java/com/gzbook/GzBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
