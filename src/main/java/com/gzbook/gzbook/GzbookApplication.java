ce8ea730280823538184384890d3bd322d2364d8
Hieu <piutar>
2020-10-07 10:13:04 +0700

    init backend

---
+++ src/main/java/com/gzbook/gzbook/GzbookApplication.java
@@ -0,0 +1,13 @@
+package com.gzbook.gzbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GzbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzbook/gzbook/GzbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzbook.gzbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
