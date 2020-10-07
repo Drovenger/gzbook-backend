026348634bff3ba0e461d4c4d7cc1a598709b681
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-10-07 10:11:30 +0700

    demo init

---
+++ src/main/java/com/gzbook/GzbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gzbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzbookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GzbookBackendApplication.class, args);
+    }
+
+}
