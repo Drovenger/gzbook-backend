7c6c4c7d6f0787f4ff2d3bb23663e947e3c68ee4
daoxuandong1997 <daoxuandong1997@gmail.con>
2020-10-08 16:28:58 +0700

    api sửa thông tin người dùng

--- src/main/java/com/gzbook/controller/UserController.java
+++ src/main/java/com/gzbook/controller/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import sun.security.util.Password;
 
@@ -33,6 +30,16 @@ public class UserController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        user.setId(id);
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.OK);
+    }
+
+    @GetMapping("findUserById/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.findUserById(id), HttpStatus.OK);
+    }
 
 
 }
