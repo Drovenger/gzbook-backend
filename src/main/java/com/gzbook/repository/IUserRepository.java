310fb7196d828feff4056ad34fdcd08ccadddff2
daoxuandong1997 <daoxuandong1997@gmail.con>
2020-10-08 11:52:35 +0700

    đã chạy, chưa test

--- src/main/java/com/gzbook/repository/IUserRepository.java
+++ src/main/java/com/gzbook/repository/IUserRepository.java
@@ -2,20 +2,23 @@ package com.gzbook.repository;
 
 import com.gzbook.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
 
 //    User findUserByUserEmail(String email);
 //
 //    Iterable<User> findUsersByUserNameContaining(String input);
 //
-//    Optional<User> findByUserName(String nickName);
-//
-//    Optional<User> findByUserEmail(String email);
+    Optional<User> findByUsername(String nickName);
+
+    Optional<User> findByEmail(String email);
 
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
+
 }
